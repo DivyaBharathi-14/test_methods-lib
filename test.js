4f3de55fa36926b9ead8824db343c42390f107f4
DivyaBharathi-14 <divyabharathi.loganathan@trustrace.com>
2020-06-15 13:50:26 +0530

    expanded test-lib

--- test.js
+++ test.js
@@ -2,27 +2,65 @@ const describe = (describe1) => {
     const expect = (actualResult) => {
         const toBe = (expectedResult) => {
             try {
-
                 if (actualResult === expectedResult) {
-                    console.log(describe1, ' :✓');
+                    console.log('✓ : ', describe1);
                 }
                 else {
-                    throw new Error();
+                    throw new Error('✗ : Test Case Failed');
                 }
             }
             catch (e) {
-                console.log(e,'Test Case Failed:✗');
+                console.log(e);
             }
         };
-        return {
-            toBe: toBe
+        const toBeTrue = () => {
+            try {
+                if (actualResult > 0) {
+                    console.log('✓ : Result is positive');
+                }
+                else {
+                    throw new Error('✗ : Result is negative');
+                }
+            }
+            catch (e) {
+                console.log(e);
+            }
         }
-    };
+        const toBeFalse = (expectedResult) => {
+            if (actualResult !== expectedResult) {
+                console.log("✗ : wrong expected result");
+            }
+        }
+        const toBeNegative = () => {
+            if (actualResult > 0) {
+                throw new Error('✗ : Expected Result should be Negative but got Positive');
+            }
+            else {
+                console.log('✓: Expected Result is Negative')
+            }
+        }
+        const toBeNull = (expectedResult) => {
+            try {
+                if (!expectedResult) {
+                    throw new Error('✗ : Null Pointer Exception');
+                }
+            }
+            catch (e) {
+                console.log(e);
+            }
+        }
+        return {
+            toBe: toBe,
+            toBeTrue: toBeTrue,
+            toBeFalse: toBeFalse,
+            toBeNegative: toBeNegative,
+            toBeNull: toBeNull
+        }
+    }
     return {
         expect: expect
     }
-
 };
 
-module.exports = describe; 
+module.exports = describe;
 
